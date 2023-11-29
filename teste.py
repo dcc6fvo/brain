c1ccb40dc328e49ec4faa4fb05acab9613edc333
felipe.volpato <fvolpato@gmail.com>
2023-11-29 14:33:39 -0300

    ok

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 import paramiko
 import time
-import Database
+import Database as Database
+import humanhash
 
 from os import chmod
 from Crypto.PublicKey import RSA
@@ -29,22 +30,31 @@ def generateSSHKeyPair():
     with open("/tmp/public.key", 'wb') as content_file:
         content_file.write(pubkey.exportKey('OpenSSH'))
 
-    with Database("localhost","brain","123456","brain") as db:
-
-        #db.execute('CREATE TABLE comments(pkey INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR, comment_body VARCHAR, date_posted TIMESTAMP)')
-        #db.execute('INSERT INTO comments (username, comment_body, date_posted) VALUES (?, ?, current_date)', ('tom', 'this is a comment'))
-        comments = db.query('SELECT * FROM comments')
-        print(comments)
-
-
 def main():
 
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect('192.168.1.193', username='suporte', password='123456')
 
-    generateSSHKeyPair()
+    #generateSSHKeyPair()
     #installDependencies(client)
+
+
+    #with Database("localhost","brain","123456","brain") as db:
+
+        #db.execute('CREATE TABLE comments(pkey INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR, comment_body VARCHAR, date_posted TIMESTAMP)')
+        #db.execute('INSERT INTO comments (username, comment_body, date_posted) VALUES (?, ?, current_date)', ('tom', 'this is a comment'))
+    #    comments = db.query('SELECT * FROM comments')
+    #    print(comments)
+
+    digest = '7528880a986c40e78c38115e640da2a1'
+    hash = humanhash.humanize(digest)
+    hashv2 = humanhash.humanize(digest, words=3)
+    hashuuid = humanhash.uuid()
+
+    print(hash)
+    print(hashv2)
+    print(hashuuid)
 
     client.close()
 
