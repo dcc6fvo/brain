8d4967852b2223bc6eed339eb337b3c676cb4d07
felipe.volpato <fvolpato@gmail.com>
2023-12-01 16:59:05 -0300

    add index inteligente com qrcode

--- adoption/index.php
+++ adoption/index.php
@@ -4,11 +4,70 @@ use chillerlan\QRCode\{QRCode, QROptions};
 
 require_once './vendor/autoload.php';
 
-$data   = 'otpauth://totp/test?secret=B3JX4VCVJDVNXNZ5&issuer=chillerlan.net';
-$qrcode = (new QRCode)->render($data);
+function firstDiskUUID() {
+    $uuids = shell_exec("blkid -o value -s UUID");  
+    $uuids_array = preg_split('/\s+/', $uuids);
+    $uuid='';
 
-// default output is a base64 encoded data URI
-printf('<img src="%s" alt="QR Code" />', $qrcode);
+    foreach ($uuids_array as &$value) {
+        if (strlen($value) == 36){
+            $uuid=$value;
+            break;
+        }
+    }
+    return $uuid;
+}
 
+function generateQRCode($uuid){
+    $data   = $uuid;
+    $qrcode = (new QRCode)->render($data);
+    return $qrcode;
+}
 
+function humanhash($uuid) {
+    return before('-', $uuid);
+}
+
+function before($a, $b){
+    return substr($b, 0, strpos($b, $a));
+}
+
+$uuid = firstDiskUUID();
+$humanhash = humanhash($uuid);
+$qrcode = generateQRCode($humanhash);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles/reset.css" />
+    <link rel="stylesheet" type="text/css" href="styles/index.css" />
+    <title>Document</title>
+</head>
+
+<body>
+
+    <section id="main" name="main" class="container">
+
+        <h1>
+            Awaiting Adoption
+        </h1>
+
+        <h2>
+            <?php
+            echo strtoupper($humanhash);
+            ?>
+        </h2>
+
+        <?php
+        // default output is a base64 encoded data URI
+        printf('<img class="qrcode" src="%s" alt="QR Code" />', $qrcode);
+        ?>
+
+    </section>
+
+</body>
+</html>
