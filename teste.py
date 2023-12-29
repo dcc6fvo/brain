b67f8e1fe8c3004106472779ca8b46a6da1780a7
felipe volpato <fvolpato@gmail.com>
2023-12-29 15:42:43 -0300

    task confirmation

--- teste.py
+++ teste.py
@@ -17,6 +17,11 @@ def doPost(url,adoption):
     r = requests.post(url, adoption)
     return r
 
+def doGet(url):
+
+    r = requests.get(url)
+    return r
+
 def get_hostname():
 
     return socket.gethostname()
@@ -45,31 +50,55 @@ def apt_update():
             print(e.output)
     logging.info("Thread: finishing")
 
-def start_adoption(dev_uuid, dev_hostname):
-
-    adoption = {}
-    adoption["id"] = dev_uuid
-    adoption["humanhash"] = dev_uuid[0:8]
-    adoption["surname"] = dev_hostname
-
-    print(adoption)
+def start_adoption(adoption):
 
     while True:
         r = doPost('http://127.0.0.1:8000/api/devices/adoption',adoption)
         response_code = r.status_code
         response_json = json.loads(r.text)
-        print(response_json.get("message"))
+        if(response_json.get("message") ==  'Device already adopted' ):
+            break
+        logging.info(response_json.get("message"))    
         time.sleep(2)
+
+def running_task(tasks):
+    
+    logging.info("Task: starting")
+    
+    for x in tasks:
+        command = x["command"].split()
+        #print(x["command"])
+        try:
+            print(subprocess.check_output(command))
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+    
+    logging.info("Task: finishing")
+
+def taking_tasks(adoption):
+
+    while True:
+        logging.info('taking tasks')    
+        time.sleep(2)
+
+        r = doGet('http://127.0.0.1:8000/api/devices/tasks/'+ adoption["id"])
+        response_code = r.status_code
+        response_json = json.loads(r.text)
+        #print(response_json.get("tasks"))
+        running_task(response_json.get("tasks"))
 
 def main():
 
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,datefmt="%H:%M:%S")
 
-    dev_uuid = get_uuid()
-    dev_hostname = get_hostname()
-    start_adoption(dev_uuid, dev_hostname)
-        
+    adoption = {}
+    adoption["id"] = get_uuid()
+    adoption["humanhash"] = get_uuid()[0:8]
+    adoption["surname"] = get_hostname()
+
+    start_adoption(adoption)
+    taking_tasks(adoption)
 
 
 
